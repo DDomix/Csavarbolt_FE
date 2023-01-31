b43bca640e06220204a483e4de23b5549a2d5a2c
DDomix <dominik.papp1010@gmail.com>
2023-01-31 18:30:44 +0100

    adatfeltoltes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { error } from 'console';
 
 interface Csavar{
   id: number;
@@ -47,6 +48,40 @@ class App extends Component<{}, State>{
   componentDidMount() {
     this.loadCsavarok();
   }
+
+  handleUpload = async () => {
+    const { tipusInput, hosszInput, arInput, keszletInput } = this.state;
+    if(tipusInput.trim() === '' || hosszInput<1 || arInput <1 || keszletInput<1){
+      this.setState(error)
+      return;
+    }
+
+    const adat = {
+      tipus: tipusInput,
+      hossz: hosszInput,
+      ar: arInput,
+      keszlet: keszletInput,
+    }
+
+    let response = await fetch('http://localhost:3000/api/csavarbolt', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(adat),
+    });
+
+    this.setState({ 
+      tipusInput: '',
+      hosszInput: 0,
+      arInput: 0,
+      keszletInput: 0,
+    })
+
+    await this.loadCsavarok();
+  };
+
+
 
 }
 
